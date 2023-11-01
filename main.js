9cfcd83b6d0e7220fee98981fa33a756bfb327d3
soaresgustavon <gu.soares0608@gmail.com>
2023-11-01 16:04:28 -0300

    adicionando funções ao projeto

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const ingressos = [];
+
 function addRedBorder(id){
     element = document.querySelector("#" + id);
     element.style.border = "5px solid red"
@@ -49,5 +51,16 @@ function addKeyboardEventListeners(){
 }, false)
 
 }
-addKeyboardEventListeners();
 
+function selectCard(selector){
+    var element = document.querySelector(selector);
+    element.classList.toggle("card-selected");
+    if(ingressos.includes(selector)) ingressos.pop(selector);
+    else ingressos.push(selector)
+}
+
+function showSelectedCards(){
+    if(ingressos.length > 0) alert("Ingressos Selecionados:" + ingressos);
+}
+
+addKeyboardEventListeners();
